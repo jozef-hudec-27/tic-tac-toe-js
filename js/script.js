08953403232495df76e6445e00e1e5b77930a702
Jozef Hudec <jojohudec27@gmail.com>
2023-01-21 17:03:35 +0100

    Make game playable

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 const Board = (() => {
   let turn = 0
+
+  let _areTheSame = (field1, field2, field3) => {
+    console.log(field1, field2, field3)
+
+    if (field1 == 0 || field2 == 0 || field3 == 0) {
+      return false
+    }
+
+    return field1.symbol == field2.symbol && field2.symbol == field3.symbol
+  }
+
+  let _winner = () => {
+    // diagonal top left to bottom right
+    if (_areTheSame(board[0][0], board[1][1], board[2][2])) {
+      return board[0][0]
+    }
+
+    // diagonal top right to bottom left
+    if (_areTheSame(board[0][2], board[1][1], board[2][0])) {
+      return board[0][2]
+    }
+
+    for (let i = 0; i < 3; i++) {
+      // row
+      if (_areTheSame(board[i][0], board[i][1], board[i][2])) {
+        return board[i][0]
+      }
+
+      // column
+      if (_areTheSame(board[0][i], board[1][i], board[2][i])) {
+        return board[0][i]
+      }
+    }
+  }
   
  let board = [
     [0, 0, 0],
@@ -7,32 +41,31 @@ const Board = (() => {
     [0, 0, 0]
   ] 
 
-  return { board, turn }
-})()
+  let play = (player, field) => {
+    let row = Math.floor((field - 1) / 3), col = (field - 1) % 3
+    board[row][col] = player
 
-const Display = (() => {
-  const drawBoard = () => {
-
+    if (_winner()) {
+      alert(`${_winner().name} WINS!`)
+    }
   }
 
-  return { drawBoard }
-}
-)()
+  return { board, turn, play }
+})()
 
-const Player = (name, num) => {
-  return { name, num }
+const Player = (name, symbol) => {
+  return { name, symbol }
 }
+
+const player1 = Player('p1', 'X')
+const player2 = Player('p2', 'O')
+
+const currentPlayer = () => Board.turn % 2 == 0 ? player1 : player2
 
 document.querySelectorAll('.field-btn').forEach(btn => {
   btn.addEventListener('click', () => {
-    btn.textContent = Board.turn % 2 == 0 ? 'X' : 'O'
+    btn.textContent = currentPlayer().symbol
     Board.turn++
+    Board.play(currentPlayer(), +btn.dataset.field)
   })
 })
-
-// const player1 = Player(prompt('Player 1 name: '), 1)
-// const player2 = Player(prompt('Player 2 name: '), 2)
-
-Display.drawBoard()
-
-console.log(player1, player2)
